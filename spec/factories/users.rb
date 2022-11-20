0f9c46456bfc63431214779a1e4493fbf7f0e530
Ubuntu <ubuntu@ip-172-31-41-14.ap-northeast-1.compute.internal>
2022-11-20 06:58:31 +0000

    user.followingとuser.folloersにテストを追加

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,6 +2,22 @@ FactoryBot.define do
   factory :user do
     email { "hogehoge@gmail.com" }
     password { "password" }
+    
+    trait :user_with_following do
+      after(:create) do |user|
+        3.times do
+          user.following << create(:other_user)
+        end
+      end
+    end
+    
+    trait :user_with_followers do
+      after(:create) do |user|
+        3.times do
+          user.followers << create(:other_user)
+        end
+      end
+    end
   end
   
   factory :other_user, class: User do
--- spec/models/relationship_spec.rb
+++ spec/models/relationship_spec.rb
@@ -1,5 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe Relationship, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context "followed_idに紐づくアカウントが削除された場合" do
+    let(:user){ create(:user, :user_with_following) }
+    
+    it "関連レコードも削除されること" do
+       other_user = user.following.last
+       expect{ other_user.destroy }.to change{ user.following.count}.by(-1)
+    end
+  end
+  
+  context "follower_idに紐づくアカウントが削除された場合" do
+    let(:user){ create(:user, :user_with_followers) }
+    
+    it "関連レコードも削除されること" do
+       other_user = user.followers.last
+       expect{ other_user.destroy }.to change{ user.followers.count}.by(-1)
+    end
+  end
 end
